34ae7dcee805da275e70eb2c6b873e52131b8d6f
mitcareem <mitcareem@gmail.com>
2022-07-27 15:52:12 +0530

    rest api

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ejs-todolist
+# Rest-API
+
+uses HTTP request verbs(get, post, put, delete, patch)
+also use specific patterns of routes/endpoint url
+
+# Protocols are the way how computer communicate each other... Protocols are set 
+   # some set of rules...
+
+
+   # Headers:-
+   represents the meta data about HTTP request / response 
+
+   # Port 
+
+       A port number is a way to identify a specific process 
---
+++ app.js
@@ -0,0 +1,44 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+    useNewUrlParser: true,
+});
+
+const articleSchema = {
+    title: String,
+    content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+//TODO
+
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticle) {
+        if (!err) {
+            res.send(foundArticle);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
